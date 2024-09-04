8f03e049601f82ba3682efeec8f99103fdbe93a9
Roman Strypa <romanstripa@gmail.com>
2024-09-04 22:34:06 +0000

    sops-kms-flux

--- main.tf
+++ main.tf
@@ -28,3 +28,24 @@ module "flux_bootstrap" {
   config_host       = module.gke_cluster.host
   config_token      = module.gke_cluster.token
 }
+
+module "gke-workload-identity" {
+  source              = "terraform-google-modules/kubernetes-engine/google/modules/workload-identity"
+  use_existing_k8s_sa = true
+  name                = "kustomize-controller"
+  namespace           = "flux-system"
+  project_id          = var.GOOGLE_PROJECT
+  cluster_name        = "main"
+  location            = var.GOOGLE_REGION
+  annotate_k8s_sa     = true
+  roles               = ["roles/cloudkms.cryptoKeyEncrypterDecrypter"]
+}
+
+module "kms" {
+  source          = "terraform-google-modules/kms/google"
+  project_id      = var.GOOGLE_PROJECT
+  keyring         = "sops-flux"
+  location        = "global"
+  keys            = ["sops-key-flux"]
+  prevent_destroy = false
+}
